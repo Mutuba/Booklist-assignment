b68ff934cb8ed41f066625e6ebba9879a0b7c3ad
Mutuba <danielmutubait@gmail.com>
2024-06-11 18:20:41 +0300

    Add an alert when adding and removing from reading list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,6 +23,7 @@ const App: React.FC = () => {
   const [searchResults, setSearchResults] = useState<Book[]>([]);
   const [readingList, setReadingList] = useState<Book[]>([]);
   const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
 
   useEffect(() => {
     if (data && data.books) {
@@ -35,9 +36,13 @@ const App: React.FC = () => {
     if (!readingList.some((b) => b.id === book.id)) {
       // If not, add the book to the readingList
       setReadingList([...readingList, book]);
+      // Show alert for book added to reading list
+      setShowAlert(true);
+      setAlertMessage("Book added to reading list");
     } else {
       // If the book already exists, show the alert
       setShowAlert(true);
+      setAlertMessage("Book already exists in the reading list");
     }
   };
 
@@ -45,14 +50,17 @@ const App: React.FC = () => {
     setReadingList(
       readingList.filter((book) => book.id !== readingListBook.id)
     );
+    // Show alert for book removed from reading list
+    setShowAlert(true);
+    setAlertMessage("Book removed from reading list");
   };
 
   return (
     <Container maxWidth="md">
       {showAlert && (
         <CustomAlert
-          message="Book already exists in the reading list"
-          severity="warning"
+          message={alertMessage}
+          severity="info"
           setShowAlert={setShowAlert}
         />
       )}
