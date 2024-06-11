4649288898fce72479958fb60295bd1c688e017d
Mutuba <danielmutubait@gmail.com>
2024-06-11 18:33:41 +0300

    Add test for custom divider

--- frontend/src/tests/shared/CustomDivider.test.tsx
+++ frontend/src/tests/shared/CustomDivider.test.tsx
@@ -4,13 +4,10 @@ import CustomDivider from "../../components/shared/CustomDivider";
 
 describe("CustomDivider", () => {
   test("renders a Divider component with correct props", () => {
-    // Render the CustomDivider component
     const { getByTestId } = render(<CustomDivider />);
 
-    // Get the Divider component by its test id
     const dividerElement = getByTestId("custom-divider");
 
-    // Assert that the Divider component exists
     expect(dividerElement).toBeInTheDocument();
   });
 });
