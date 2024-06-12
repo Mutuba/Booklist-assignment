ecf52c46849637423a68f8ce731d681c95003623
Mutuba <danielmutubait@gmail.com>
2024-06-12 10:43:42 +0300

    Refactor reading list tests

--- frontend/src/tests/shared/CustomAlert.test.tsx
+++ frontend/src/tests/shared/CustomAlert.test.tsx
@@ -2,27 +2,29 @@ import React from "react";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import CustomAlert from "../../components/shared/CustomAlert";
 
-test("renders alert with message and close button", async () => {
-  // Mock setShowAlert function
-  const setShowAlert = jest.fn();
+describe("CustomAlert Component", () => {
+  test("renders alert with message and close button", async () => {
+    // Mock setShowAlert function
+    const setShowAlert = jest.fn();
 
-  render(
-    <CustomAlert
-      message="Test message"
-      severity="error"
-      setShowAlert={setShowAlert}
-    />
-  );
+    render(
+      <CustomAlert
+        message="Test message"
+        severity="error"
+        setShowAlert={setShowAlert}
+      />
+    );
 
-  expect(screen.getByRole("alert")).toHaveTextContent("Test message");
+    expect(screen.getByRole("alert")).toHaveTextContent("Test message");
 
-  const closeButton = screen.getByLabelText("close");
-  expect(closeButton).toBeInTheDocument();
+    const closeButton = screen.getByLabelText("close");
+    expect(closeButton).toBeInTheDocument();
 
-  fireEvent.click(closeButton);
+    fireEvent.click(closeButton);
 
-  // Wait for the alert to close
-  await waitFor(() => {
-    expect(setShowAlert).toHaveBeenCalledWith(false);
+    // Wait for the alert to close
+    await waitFor(() => {
+      expect(setShowAlert).toHaveBeenCalledWith(false);
+    });
   });
 });
--- frontend/src/tests/shared/CustomDivider.test.tsx
+++ frontend/src/tests/shared/CustomDivider.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import CustomDivider from "../../components/shared/CustomDivider";
 
-describe("CustomDivider", () => {
+describe("CustomDivider Component", () => {
   test("renders a Divider component with correct props", () => {
     const { getByTestId } = render(<CustomDivider />);
 
